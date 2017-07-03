bb7ae2c2eac0d593afefaedd78ee59bdf104c214
Arun <arun.cse.psn@gmail.com>
2017-07-03 16:54:55 +0530

    Initial Commit

---
+++ SampleGithub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleGithub
+//
+//  Created by SRMTech on 03/07/17.
+//  Copyright © 2017 SRM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
